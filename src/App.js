eadbba0103c81dd6eb967bc4b65509778c1d6dfe
Jeffrey <aligula.jeff@gmail.com>
2023-02-10 09:35:01 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  //Let us fetch the data from the backend
+  const [data, setData] = useState(null);
+  
+  useEffect(() => {
+    fetch('/api')
+      .then((res) => res.text())
+      .then((data) => setData(data));
+  }, []);
+
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+          {/* show the data from the backend */}
+          <p>{!data ? "Loading..." : data}</p>
+        
+      
       </header>
     </div>
   );
